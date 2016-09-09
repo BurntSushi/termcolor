8f0d3d78ca96615e1afe4d39cb4699d957be31a9
Andrew Gallant <jamslam@gmail.com>
2016-09-09 18:10:20 -0400

    clean up CI script

--- ci/script.sh
+++ ci/script.sh
@@ -11,42 +11,20 @@ disable_cross_doctests() {
         if [ "$TRAVIS_OS_NAME" = "osx" ]; then
             brew install gnu-sed --default-names
         fi
-
         find src -name '*.rs' -type f | xargs sed -i -e 's:\(//.\s*```\):\1 ignore,:g'
     fi
 }
 
-# TODO modify this function as you see fit
-# PROTIP Always pass `--target $TARGET` to cargo commands, this makes cargo output build artifacts
-# to target/$TARGET/{debug,release} which can reduce the number of needed conditionals in the
-# `before_deploy`/packaging phase
 run_test_suite() {
-    case $TARGET in
-        # configure emulation for transparent execution of foreign binaries
-        aarch64-unknown-linux-gnu)
-            export QEMU_LD_PREFIX=/usr/aarch64-linux-gnu
-            ;;
-        arm*-unknown-linux-gnueabihf)
-            export QEMU_LD_PREFIX=/usr/arm-linux-gnueabihf
-            ;;
-        *)
-            ;;
-    esac
-
-    if [ ! -z "$QEMU_LD_PREFIX" ]; then
-        # Run tests on a single thread when using QEMU user emulation
-        export RUST_TEST_THREADS=1
-    fi
-
     cargo build --target $TARGET --verbose
-    cargo test --target $TARGET
+    cargo test --target $TARGET --verbose
 
     # sanity check the file type
     file target/$TARGET/debug/rg
 }
 
 main() {
-    disable_cross_doctests
+    # disable_cross_doctests
     run_test_suite
 }
 
