14c194dbe1f6705781919c472d618b0d16390f17
Andrew Gallant <jamslam@gmail.com>
2016-09-22 20:07:43 -0400

    Update brew formula.
    We have a man page on OSX. Yay!

--- pkg/brew/ripgrep.rb
+++ pkg/brew/ripgrep.rb
@@ -1,18 +1,19 @@
 require  'formula'
 class Ripgrep < Formula
-  version '0.1.14'
+  version '0.1.15'
   desc "Search tool like grep and The Silver Searcher."
   homepage "https://github.com/BurntSushi/ripgrep"
 
   if Hardware::CPU.is_64_bit?
     url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "dbbe1099ca6e6381a0585c17718b31e266b0c7426fc0343a096677c3efab9777"
+    sha256 "fc138cd57b533bd65739f3f695322e483fe648736358d853ddb9bcd26d84fdc5"
   else
     url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-i686-apple-darwin.tar.gz"
-    sha256 "41c2c43a69afddc6205257b38f50f42ad6424d5d7e21720de5cc1bde3849921c"
+    sha256 "3ce1f12e49a463bc9dd4cfe2537aa9989a0dc81f7aa6f959ee0d0d82b5f768cb"
   end
 
   def install
     bin.install "rg"
+    man1.install "rg.1"
   end
 end
