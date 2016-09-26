2b15832655539ce5550244621680fd1fe9cd3795
Andrew Gallant <jamslam@gmail.com>
2016-09-25 22:50:50 -0400

    update brew formula to 0.2.0

--- pkg/brew/ripgrep.rb
+++ pkg/brew/ripgrep.rb
@@ -1,15 +1,15 @@
 require  'formula'
 class Ripgrep < Formula
-  version '0.1.17'
+  version '0.2.0'
   desc "Search tool like grep and The Silver Searcher."
   homepage "https://github.com/BurntSushi/ripgrep"
 
   if Hardware::CPU.is_64_bit?
     url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-apple-darwin.tar.gz"
-    sha256 "cb7b551a08849cef6ef8f17229224f094299692981976a3c5873c93f68c8fa1a"
+    sha256 "f55ef5dac04178bcae0d6c5ba2d09690d326e8c7c3f28e561025b04e1ab81d80"
   else
     url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-i686-apple-darwin.tar.gz"
-    sha256 "0e936874b9f3fd661c5566e7f8fe18343baa5e9371e57d8d71000e9234fc376b"
+    sha256 "d901d55ccb48c19067f563d42652dfd8642bf50d28a40c0e2a4d3e866857a93b"
   end
 
   def install
