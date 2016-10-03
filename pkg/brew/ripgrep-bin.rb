95f552fc069b6cacdcdd7a3c7eff4dacb0965325
Colin Kennedy <moshen.colin@gmail.com>
2016-10-03 16:39:34 -0500

    Fix brew formula name to not conflict with core
    Since the homebrew-core formula was accepted, we should differentiate
    the prebuilt formula available in this tap

--- pkg/brew/ripgrep-bin.rb
+++ pkg/brew/ripgrep-bin.rb
@@ -1,9 +1,11 @@
-class Ripgrep < Formula
+class RipgrepBin < Formula
   version '0.2.1'
   desc "Search tool like grep and The Silver Searcher."
   homepage "https://github.com/BurntSushi/ripgrep"
   url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-apple-darwin.tar.gz"
   sha256 "f8b208239b988708da2e58f848a75bf70ad144e201b3ed99cd323cc5a699625f"
+
+  conflicts_with "ripgrep"
 
   def install
     bin.install "rg"
