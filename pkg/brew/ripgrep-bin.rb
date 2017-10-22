311ccb1f6b5a2bfe83ec0ceafd357c51501158fc
Andrew Gallant <jamslam@gmail.com>
2017-10-22 08:08:49 -0400

    update brew tap

--- pkg/brew/ripgrep-bin.rb
+++ pkg/brew/ripgrep-bin.rb
@@ -1,9 +1,9 @@
 class RipgrepBin < Formula
-  version '0.6.0'
+  version '0.7.0'
   desc "Search tool like grep and The Silver Searcher."
   homepage "https://github.com/BurntSushi/ripgrep"
   url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "2aeffe25322f886bcd846ac15b6574dc769865fb44b28a9b0c356f3004019685"
+  sha256 "ef5e76acde453bed774cecaeef8dbd96652d616d957ec1763d450c6f6d9e1d0d"
 
   conflicts_with "ripgrep"
 
