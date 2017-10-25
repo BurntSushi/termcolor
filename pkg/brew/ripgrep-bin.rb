a9377da624d8efde460cfb032456a70bb2b24835
Francois Ferrand <francois.ferrand@parrot.com>
2017-10-25 13:35:24 +0200

    Support ripgrep-bin installation with linuxbrew

--- pkg/brew/ripgrep-bin.rb
+++ pkg/brew/ripgrep-bin.rb
@@ -2,8 +2,14 @@ class RipgrepBin < Formula
   version '0.7.1'
   desc "Search tool like grep and The Silver Searcher."
   homepage "https://github.com/BurntSushi/ripgrep"
-  url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-apple-darwin.tar.gz"
-  sha256 "ee670b0fba46323ee9a2d1c5b8bee46fa3e45778f6f105f2e8e9ee29e8bd0d45"
+
+  if OS.mac?
+      url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-apple-darwin.tar.gz"
+      sha256 "ee670b0fba46323ee9a2d1c5b8bee46fa3e45778f6f105f2e8e9ee29e8bd0d45"
+  elsif OS.linux?
+      url "https://github.com/BurntSushi/ripgrep/releases/download/#{version}/ripgrep-#{version}-x86_64-unknown-linux-musl.tar.gz"
+      sha256 "ac595c2239b9a30e0e0744578afa6b73e32cdd8ae61d4f1c0ee5d6b55adbadcf"
+  end
 
   conflicts_with "ripgrep"
 
